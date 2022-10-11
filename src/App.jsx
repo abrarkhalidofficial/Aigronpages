e47b5bad65cc7a48c0f1e01f55f9a2fa5bfc1629
M Abrar Khalid <abrarprince471@gmail.com>
2022-10-11 18:19:05 +0500

    page done

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,37 @@ import { Footer } from "./components/Footer";
 
 import "./App.scss";
 import "react-phone-number-input/style.css";
+import careerimg from "./assets/career.png";
+import carrescontentright from "./assets/career-content-right.png";
 
 function App() {
   return (
     <>
       <Header />
-      <HomeSection />
-      <IdeaSection />
-      <ServicesSection />
-      <ExpertiseSection />
-      <OfficeSection />
-      <ContactSection />
+      <section className="Career__page">
+        <div className="career__page__img">
+          <img src={careerimg} alt="career" />
+        </div>
+        <div className="career__page__heading">Career</div>
+      </section>
+      <section className="Career__page__content__section">
+        <div className="Career__page__content__left">
+          <div className="Career__page__content__left__heading">
+            Want to become part of the Aigron family?
+          </div>
+          <div className="Career__page__content__left__info">
+            Get in touch for our latest job openings.
+          </div>
+          <div className="Career__page__content__left__button">
+            <button>Contact Us</button>
+          </div>
+        </div>
+        <div className="Career__page__content__right">
+          <div className="Career__page__content__right__img">
+            <img src={carrescontentright} alt="carrescontentright" />
+          </div>
+        </div>
+      </section>
       <Footer />
     </>
   );
